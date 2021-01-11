3439aaf95a54be036e342ff39d074de650b2ccdb
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-01-11 21:39:15 +0000

    (SIMP-8958) Standardize assets in pupmod-simp-oath
    This patch baselines the latest standardized assets for Puppet modules.
    
    SIMP-9053 #close
    [SIMP-8958] #comment Standardized assets in pupmod-simp-oath
    [SIMP-8839] #comment Removed EL6 from pupmod-simp-oath
    [SIMP-8489] #comment Updated pupmod-simp-oath GLCI pipeline to Puppet 6.18
    [SIMP-8923] #comment Renamed 'sanity' to 'releng' in pupmod-simp-oath
    [SIMP-8984] #comment Updated pupmod-simp-oath to new GLCI conventions

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,10 +9,4 @@ class oath::install {
   package { 'liboath': ensure  => $::oath::package_ensure }
   package { 'pam_oath': ensure => $::oath::package_ensure }
 
-  if $facts['os']['release']['major'] == '6' {
-    file { '/lib64/security/pam_oath.so':
-      ensure => 'link',
-      target => '/usr/lib64/security/pam_oath.so'
-    }
-  }
 }
